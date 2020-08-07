ebdd9ac04bdc54e86e13458f2c67f567935e4af1
Sergio <sergey.dko@gmail.com>
2020-08-07 18:11:03 +0200

    Updates

---
+++ precache-manifest.9f2a66927fccb1b7ac9abcfad7a49eb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a781d4fb115e6202cdd5fbbb6dc2044",
+    "url": "/face-recognition-app/index.html"
+  },
+  {
+    "revision": "d2922a7fe748746bb37d",
+    "url": "/face-recognition-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "553adfa8dfb9b43c02b9",
+    "url": "/face-recognition-app/static/css/main.8f57d800.chunk.css"
+  },
+  {
+    "revision": "d2922a7fe748746bb37d",
+    "url": "/face-recognition-app/static/js/2.84ba8937.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recognition-app/static/js/2.84ba8937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "553adfa8dfb9b43c02b9",
+    "url": "/face-recognition-app/static/js/main.c6581eb3.chunk.js"
+  },
+  {
+    "revision": "c22eb5eba8663bd91f02",
+    "url": "/face-recognition-app/static/js/runtime-main.a073d0dd.js"
+  }
+]);
